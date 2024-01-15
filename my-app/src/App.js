5732b554b7acc1c4398eda5704862552f45e4717
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-15 16:03:59 +0000

    Add code snippet of UseStateWithObjects.js file

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import ControlledFormsHooks from "./components/ControlledFormsHooks";
 import UseStatesWithArrays from './components/UseStatesWithArrays';
+import UseStateWithObjects from './components/UseStateWithObjects';
 
 function App() {
   return (
@@ -9,7 +10,9 @@ function App() {
       {/* lecture 1 hooks below */}
       {/* <ControlledFormsHooks /> */}
       {/* lecture 2 hooks below */}
-      <UseStatesWithArrays />
+      {/* <UseStatesWithArrays /> */}
+      {/* lecture 3 hooks below */}
+      <UseStateWithObjects />
 
     </div>
   );
