4aaa06b185fd1b71fb74f691d15d06534e94078c
Muhammad Bilal <bilalqureshi.epic@gmail.com>
2024-01-15 13:17:12 +0000

    Copy controlled form jsx to controlled form hooks, this would not work right now

--- my-app/src/components/ControlledFormsHooks.js
+++ my-app/src/components/ControlledFormsHooks.js
@@ -3,8 +3,44 @@ import React from 'react'
 function ControlledFormsHooks() {
   return (
     <div>
-        It works
-    </div>
+                <h2>Please fill out the form below:</h2>
+                <form onSubmit={this.handleSubmit}>
+                    <div>
+                        <label htmlFor="id-name">Your Name:</label>
+                        <input
+                            value={this.state.name}
+                            onChange={this.handleNameChange}
+                            id="id-name"
+                            name="name"
+                            type="text"
+                        />
+                    </div>
+                    <div>
+                        <label htmlFor="id-category">Query category:</label>
+                        <select
+                            id="id-category"
+                            name="category"
+                            value={this.state.category}
+                            onChange={this.handleCategoryChange}
+                        >
+                            <option value="website">Website issue</option>
+                            <option value="order">Order issue</option>
+                            <option value="general">General inquiry</option>
+
+                        </select>
+                    </div>
+                    <div>
+                        <label htmlFor="id-comments">Comments:</label>
+                        <textarea
+                            id="id-comments"
+                            name="comments"
+                            value={this.state.comments}
+                            onChange={this.handleCommentsChange}
+                        />
+                    </div>
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
   )
 }
 
